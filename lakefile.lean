ba2f919d6a14f047483815e16b82c6fe422f3bf3
GhostDriftTheory <hide.pollini.kenkou@gmail.com>
2026-05-16 13:03:39 +0900

    Rename package and default target in lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,12 @@
 import Lake
 open Lake DSL
 
-package «adic_lean_proof_replay» where
+package «adic_lean_qdd_replay» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
 
-@[default_target]
 lean_lib «ADIC_RSound_Replay» where
+
+@[default_target]
+lean_lib «ADIC_QuantumDrugDiscovery» where
